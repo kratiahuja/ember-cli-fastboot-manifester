50da17f0f2e844f43f71bb4eebde1e85b2fd1564
Krati Ahuja <kratiahuja@gmail.com>
2017-04-21 15:20:04 -0700

    Manifest builder

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-fastboot-manifester'
+  name: 'ember-cli-fastboot-manifester',
+
+  updateFastBootManifest: function(manifest) {
+    manifest.vendorFiles.unshift('ember-cli-fastboot-manifester/cat.js');
+    manifest.appFiles.push('ember-cli-fastboot-manifester/dog.js');
+
+    return manifest;
+  }
 };
